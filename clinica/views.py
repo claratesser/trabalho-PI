544cc8e63b8f09044381f4e8f39a877ddcdc81d3
claratesser <claratesserc@gmail.com>
2025-07-04 16:42:03 -0300

    FEAT: criação das views #7

--- clinica/views.py
+++ clinica/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Paciente, Especialidade, Consulta, Medicamento, Medico, Receita_medicamento, Receita
+from .serializers import EstadoSerializer
 
-# Create your views here.
+class EstadoViewSet(ModelViewSet):
+    queryset = Paciente.objects.all()
+    serializer_class = EstadoSerializer
+
+class EstadoViewSet(ModelViewSet):
+    queryset = Especialidade.objects.all()
+    serializer_class = EstadoSerializer
+
+class EstadoViewSet(ModelViewSet):
+    queryset = Consulta.objects.all()
+    serializer_class = EstadoSerializer
+
+class EstadoViewSet(ModelViewSet):
+    queryset = Medicamento.objects.all()
+    serializer_class = EstadoSerializer
+
+class EstadoViewSet(ModelViewSet):
+    queryset = Medico.objects.all()
+    serializer_class = EstadoSerializer
+
+class EstadoViewSet(ModelViewSet):
+    queryset = Receita_medicamento.objects.all()
+    serializer_class = EstadoSerializer
+
+class EstadoViewSet(ModelViewSet):
+    queryset = Receita.objects.all()
+    serializer_class = EstadoSerializer
