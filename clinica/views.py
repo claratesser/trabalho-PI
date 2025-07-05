4ca87fe90d0ae0b700d2ba8067d885a35eeab8ea
claratesser <claratesserc@gmail.com>
2025-07-05 09:18:51 -0300

    FEAT: correção das views #7

--- clinica/views.py
+++ clinica/views.py
@@ -5,28 +5,28 @@ from .serializers import EstadoSerializer
 
 class PacienteViewSet(ModelViewSet):
     queryset = Paciente.objects.all()
-    serializer_class = EstadoSerializer
+    serializer_class = PacienteSerializer
 
 class EspecialidadeViewSet(ModelViewSet):
     queryset = Especialidade.objects.all()
-    serializer_class = EstadoSerializer
+    serializer_class = EspecialidadeSerializer
 
 class ConsultaViewSet(ModelViewSet):
     queryset = Consulta.objects.all()
-    serializer_class = EstadoSerializer
+    serializer_class = ConsultaSerializer
 
 class MedicamentoViewSet(ModelViewSet):
     queryset = Medicamento.objects.all()
-    serializer_class = EstadoSerializer
+    serializer_class = MedicamentoSerializer
 
 class MedicoViewSet(ModelViewSet):
     queryset = Medico.objects.all()
-    serializer_class = EstadoSerializer
+    serializer_class = MedicoSerializer
 
 class Receita_medicamentoViewSet(ModelViewSet):
     queryset = Receita_medicamento.objects.all()
-    serializer_class = EstadoSerializer
+    serializer_class = Receita_medicamentoSerializer
 
 class ReceitaViewSet(ModelViewSet):
     queryset = Receita.objects.all()
-    serializer_class = EstadoSerializer
+    serializer_class = ReceitaSerializer
